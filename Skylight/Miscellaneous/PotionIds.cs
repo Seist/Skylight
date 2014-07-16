b71391d2147c2e98be6577816016aefd78a61f6c
Decagon <decagongithub@gmail.com>
2014-07-16 16:51:20 -0300

    Docs and var consistency for potion ids

--- Skylight/Miscellaneous/PotionIds.cs
+++ Skylight/Miscellaneous/PotionIds.cs
@@ -1,30 +1,45 @@
-﻿namespace Skylight.Miscellaneous
+﻿// ***********************************************************************
+// <copyright file="PotionIds.cs" company="">
+//     Copyright 2014 (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Skylight.Miscellaneous
 {
+    /// <summary>
+    /// Class PotionIds.
+    /// </summary>
     public static class PotionIds
     {
+        /// <summary>
+        /// The curse
+        /// </summary>
         public static readonly int
-            CURSE = 6,
-            INVULNERABLE = 8,
-            ZOMBIE = 9,
-            RESPAWN = 10,
-            FLAUNTLEVEL = 12,
-            SOLITUDE = 13;
+            Curse = 6, Invulnerable = 8, Zombie = 9, Respawn = 10, Flauntlevel = 12, Solitude = 13;
 
+        /// <summary>
+        /// Class Auras.
+        /// </summary>
         public static class Auras
         {
+            /// <summary>
+            /// The aura colors
+            /// </summary>
             public static readonly int
-                RED = 1,
-                BLUE = 2,
-                YELLOW = 3,
-                GREEN = 5,
-                FIRE = 7;
+                Red = 1, Blue = 2, Yellow = 3, Green = 5, Fire = 7;
         }
 
+        /// <summary>
+        /// Class GravityModifiers.
+        /// </summary>
         public static class GravityModifiers
         {
+            /// <summary>
+            /// The jumpboost class
+            /// </summary>
             public static readonly int
-                JUMPBOOST = 4,
-                LEVITATION = 11;
+                Jumpboost = 4, Levitation = 11;
         }
     }
 }
