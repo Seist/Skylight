cd1f72b3af2144a0142fa3a4fa33241cc0262ef3
Decagon <decagongithub@gmail.com>
2014-07-16 16:56:39 -0300

    Var consistency for smileyids

--- Skylight/Miscellaneous/SmileyIds.cs
+++ Skylight/Miscellaneous/SmileyIds.cs
@@ -20,12 +20,12 @@ namespace Skylight.Miscellaneous
             /// The types of beta smilies.
             /// </summary>
             public const int
-                TEAR = 6,
-                WINK = 7,
-                SUPERFRUSTRATED = 8,
-                SHADES = 9,
-                DEVIL = 10,
-                BEWILDERED = 11;
+                Tear = 6,
+                Wink = 7,
+                Superfrustrated = 8,
+                Shades = 9,
+                Devil = 10,
+                Bewildered = 11;
         }
 
         /// <summary>
@@ -37,10 +37,10 @@ namespace Skylight.Miscellaneous
             /// The types of smilies available for contest winners.
             /// </summary>
             public const int
-                BIRD = 29,
-                BUNNY = 30,
-                PUMPKIN = 44,
-                LITPUMPKIN = 45;
+                Bird = 29,
+                Bunny = 30,
+                Pumpkin = 44,
+                Litpumpkin = 45;
         }
 
         /// <summary>
@@ -52,13 +52,13 @@ namespace Skylight.Miscellaneous
             /// The smilies that are included by default.
             /// </summary>
             public const int
-                SMILE = 0,
-                BIGSMILE = 1,
-                TONGUE = 2,
-                SQUINTHAPPY = 3,
-                FRUSTRATED = 4,
-                FROWN = 5,
-                INDIFFERENT = 18;
+                Smile = 0,
+                Bigsmile = 1,
+                Tongue = 2,
+                Squinthappy = 3,
+                Frustrated = 4,
+                Frown = 5,
+                Indifferent = 18;
         }
 
         /// <summary>
@@ -70,69 +70,69 @@ namespace Skylight.Miscellaneous
             /// The smilies that can be bought using the shop.
             /// </summary>
             public const int
-                NINJA = 12,
-                SANTA = 13,
-                WORKER = 14,
-                TOPHAT = 15,
-                SURPRISED = 17,
-                FEMALE = 18,
-                NEWYEARS2010 = 19,
-                COY = 20,
-                TERMINATOR = 24,
-                XD = 25,
-                BULLY = 26,
-                COMMANDO = 27,
-                KISS = 28,
-                XP = 33,
-                MAILMAN = 34,
-                KNIGHT = 35,
-                ANGEL = 36,
-                NURSE = 37,
-                VAMPIRE = 38,
-                GHOST = 39,
-                FRANKENSTEIN = 40,
-                NATIVEAMERICAN = 42,
-                PILGRIM = 43,
-                SNOWMAN = 46,
-                REINDEER = 47,
-                MAESTRO = 49,
-                BORED = 51,
-                ROBBER = 52,
-                POLICEMAN = 53,
-                PIRATE = 55,
-                VIKING = 56,
-                KARATE = 57,
-                COWBOY = 58,
-                SCUBADIVER = 59,
-                SUNTANNED = 60,
-                PROPELLER = 61,
-                HARDHAT = 62,
-                GASMASK = 63,
-                ROBOT = 64,
-                PEASANT = 65,
-                SOLDIER = 66,
-                BLACKSMITH = 67,
-                LOL = 68,
-                DOG = 69,
-                ALIEN = 70,
-                ASTRONAUT = 71,
-                CANNONBALL = 76,
-                MONSTER = 77,
-                SKULL = 78,
-                MADSCIENTIST = 79,
-                HEADHUNTER = 80,
-                SAFARI = 81,
-                RANGER = 82,
-                NEWYEARS2013 = 83,
-                SNOWHAT = 84,
-                FLAME = 85,
-                BISHOP = 86,
-                BRUCE = 88,
-                UNIT = 89,
-                SPARTAN = 90,
-                HELEN = 91,
-                COW = 92,
-                FARMER = 93;
+                Ninja = 12,
+                Santa = 13,
+                Worker = 14,
+                Tophat = 15,
+                Surprised = 17,
+                Female = 18,
+                Newyears2010 = 19,
+                Coy = 20,
+                Terminator = 24,
+                Xd = 25,
+                Bully = 26,
+                Commando = 27,
+                Kiss = 28,
+                Xp = 33,
+                Mailman = 34,
+                Knight = 35,
+                Angel = 36,
+                Nurse = 37,
+                Vampire = 38,
+                Ghost = 39,
+                Frankenstein = 40,
+                Nativeamerican = 42,
+                Pilgrim = 43,
+                Snowman = 46,
+                Reindeer = 47,
+                Maestro = 49,
+                Bored = 51,
+                Robber = 52,
+                Policeman = 53,
+                Pirate = 55,
+                Viking = 56,
+                Karate = 57,
+                Cowboy = 58,
+                Scubadiver = 59,
+                Suntanned = 60,
+                Propeller = 61,
+                Hardhat = 62,
+                Gasmask = 63,
+                Robot = 64,
+                Peasant = 65,
+                Soldier = 66,
+                Blacksmith = 67,
+                Lol = 68,
+                Dog = 69,
+                Alien = 70,
+                Astronaut = 71,
+                Cannonball = 76,
+                Monster = 77,
+                Skull = 78,
+                Madscientist = 79,
+                Headhunter = 80,
+                Safari = 81,
+                Ranger = 82,
+                Newyears2013 = 83,
+                Snowhat = 84,
+                Flame = 85,
+                Bishop = 86,
+                Bruce = 88,
+                Unit = 89,
+                Spartan = 90,
+                Helen = 91,
+                Cow = 92,
+                Farmer = 93;
         }
 
         /// <summary>
@@ -144,17 +144,17 @@ namespace Skylight.Miscellaneous
             /// The special smilies that cannot be bought.
             /// </summary>
             public const int
-                SUPERMAN = 16,
-                REDWIZARD = 32,
-                BLUEWIZARD = 21,
-                ULTIMATEFAN = 22,
-                WITCH = 41,
-                GRINCH = 48,
-                ORANGEPARTY = 72,
-                GREENPARTY = 73,
-                BLUEPARTY = 74,
-                REDPARTY = 75,
-                ZOMBIE = 87;
+                Superman = 16,
+                Redwizard = 32,
+                Bluewizard = 21,
+                Ultimatefan = 22,
+                Witch = 41,
+                Grinch = 48,
+                Orangeparty = 72,
+                Greenparty = 73,
+                Blueparty = 74,
+                Redparty = 75,
+                Zombie = 87;
         }
 
         /// <summary>
@@ -166,8 +166,8 @@ namespace Skylight.Miscellaneous
             /// The smilies that may or may not exist.
             /// </summary>
             public const int
-                DJ = 50,
-                UPSIDEDOWN = 54;
+                Dj = 50,
+                Upsidedown = 54;
         }
     }
 }
