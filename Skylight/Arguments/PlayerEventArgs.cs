85f8c4c2323d24527b68da08e5a1fd9f33e52440
Decagon <decagongithub@gmail.com>
2014-07-16 09:52:41 -0300

    Added docs

--- Skylight/Arguments/PlayerEventArgs.cs
+++ Skylight/Arguments/PlayerEventArgs.cs
@@ -3,33 +3,29 @@ using PlayerIOClient;
 
 namespace Skylight.Arguments
 {
+    /// <summary>
+    /// This class sets the properties about a player such as who the player is (username), their most
+    /// recent message and where they are (the room that they are in).
+    /// </summary>
     public class PlayerEventArgs : EventArgs
     {
-        private readonly Room _origin;
-
-        private readonly Message _rawMessage;
-        private readonly Player _subject;
-
+        /// <summary>
+        /// The main constructor method.
+        /// </summary>
+        /// <param name="subject">The player</param>
+        /// <param name="origin">The room where the player is originating in.</param>
+        /// <param name="rawMessage">The raw, unparsed message from the server (concerning player).</param>
         public PlayerEventArgs(Player subject, Room origin, Message rawMessage)
         {
-            _subject = subject;
-            _origin = origin;
-            _rawMessage = rawMessage;
+            Subject = subject;
+            Origin = origin;
+            RawMessage = rawMessage;
         }
 
-        public Player Subject
-        {
-            get { return _subject; }
-        }
+        public Player Subject { get; private set; }
 
-        public Room Origin
-        {
-            get { return _origin; }
-        }
+        public Room Origin { get; private set; }
 
-        public Message RawMessage
-        {
-            get { return _rawMessage; }
-        }
+        public Message RawMessage { get; private set; }
     }
 }
