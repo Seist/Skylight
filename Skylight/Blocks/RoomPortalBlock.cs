86e2d36455c9d61de1022d7949cb1294c3ca3947
Decagon <decagongithub@gmail.com>
2014-07-16 16:48:40 -0300

    Docs for roomportalblock
    Seems like there's some duplication going on here.

--- Skylight/Blocks/RoomPortalBlock.cs
+++ Skylight/Blocks/RoomPortalBlock.cs
@@ -1,12 +1,29 @@
-﻿using System.Text.RegularExpressions;
+﻿// <copyright file="RoomPortalBlock.cs" company="">
+//     Copyright 2014 (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Text.RegularExpressions;
 using Skylight.Miscellaneous;
 
 namespace Skylight.Blocks
 {
+    /// <summary>
+    /// Class RoomPortalBlock.
+    /// </summary>
     public class RoomPortalBlock : Block
     {
+        /// <summary>
+        /// The portal destination
+        /// </summary>
         private string _portalDestination;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoomPortalBlock"/> class.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="portalDestination">The portal destination.</param>
         public RoomPortalBlock(
             int x,
             int y,
@@ -15,6 +32,10 @@ namespace Skylight.Blocks
             PortalDestination = portalDestination;
         }
 
+        /// <summary>
+        /// Gets the portal destination.
+        /// </summary>
+        /// <value>The portal destination.</value>
         public string PortalDestination
         {
             get { return _portalDestination; }
