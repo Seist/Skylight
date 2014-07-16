94b90fbdbe3c88583df6a1629f96b9296f2113b8
Decagon <example@user.com>
2014-07-15 21:37:11 -0300

    Have to fix this.

--- Skylight/Arguments/BlockEventArgs.cs
+++ Skylight/Arguments/BlockEventArgs.cs
@@ -1,20 +1,48 @@
-﻿
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlockEventArgs.cs" company="TakoMan02">
+//     Copyright TakoMan02
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Skylight
 {
     using System;
 
+    /// <summary>
+    /// Sends the delegate an event based on the content of the block.
+    /// </summary>
     public class BlockEventArgs : EventArgs
     {
+        /// <summary>
+        /// The originating room
+        /// </summary>
         private Room origin;
 
+        /// <summary>
+        /// The block.
+        /// </summary>
         private Block placed;
 
+        /// <summary>
+        /// A player object containing who was the author of the block.
+        /// </summary>
         private Player placer;
+<<<<<<< HEAD
         
         public BlockEventArgs(Block b, Room origin)
         {
             this.origin = origin;
+=======
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlockEventArgs" />. Send a block changed event.
+        /// </summary>
+        /// <param name="b">The block</param>
+        /// <param name="origin">The room where the block originated from.</param>
+        public BlockEventArgs(Block b, Room origin = null)
+        {
+            this.origin = origin ?? Bot.currentRoom;
+>>>>>>> origin/master
             this.placed = b;
             this.placer = b.Placer;
         }
