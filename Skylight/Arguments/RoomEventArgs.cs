f326b3f0787fc61e5d808538e5c9f3cc58089fed
Decagon <decagongithub@gmail.com>
2014-07-16 21:17:55 -0300

    Docs for RoomEventArgs

--- Skylight/Arguments/RoomEventArgs.cs
+++ Skylight/Arguments/RoomEventArgs.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿// <copyright file="RoomEventArgs.cs" company="">
+//     Copyright 2014 (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 
 namespace Skylight.Arguments
 {
+    /// <summary>
+    /// Class RoomEventArgs.
+    /// </summary>
     public class RoomEventArgs : EventArgs
     {
+        /// <summary>
+        /// The changed room
+        /// </summary>
         private readonly Room _changedRoom;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoomEventArgs"/> class.
+        /// </summary>
+        /// <param name="changedRoom">The changed room.</param>
         public RoomEventArgs(Room changedRoom)
         {
             _changedRoom = changedRoom;
         }
 
+        /// <summary>
+        /// Gets the changed room.
+        /// </summary>
+        /// <value>The changed room.</value>
         public Room ChangedRoom
         {
             get { return _changedRoom; }
