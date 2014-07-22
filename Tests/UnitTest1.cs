e6f9f9d1640d74d1bad4e590f52914f88ef49089
Decagon <decagongithub@gmail.com>
2014-07-22 17:31:20 -0300

    Adding the unit test project to Skylight

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skylight;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // regular tests
+            Assert.AreEqual("P8HBlHZmahdz",Skylight.Tools.ParseUrl("http://everybodyedits.com/games/P8HBlHZmahdz"));
+            Assert.AreEqual("PWmSN5eRsWbkI", Skylight.Tools.ParseUrl("http://everybodyedits.com/games/PWmSN5eRsWbkI"));
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("http://everybodyedits.com/games/PWUQP6-FgpbkI"));
+        
+            // too much whitespace
+
+            Assert.AreEqual("PWEYhVZmR3bUI", Skylight.Tools.ParseUrl("   http://everybodyedits.com/games / PWEYhVZmR3bUI     "));
+            Assert.AreEqual("PW5HSvgjKybUI", Skylight.Tools.ParseUrl("http:// everybodyedits.com/games/PW5HSvgjKybUI                          "));
+            
+            // malformed url
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("http://everybodyeditz4.com/games/PWUQP6-FgpbkI"));
+            Assert.AreEqual("PWUQP6FgpbkI", Skylight.Tools.ParseUrl("https://everybodyedits.com/games/PWUQP6FgpbkI"));
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("hello://example.com/gazdsadasmes/PWUQP6-FgpbkI"));
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("http://nothinghellocool.ca/PWUQP6-FgpbkI"));
+
+            // missing urls but valid room names
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("PWUQP6-FgpbkI"));
+            Assert.AreEqual("PWUQP6-FgpbkI", Skylight.Tools.ParseUrl("  PWUQP6      -FgpbkI       "));
+        }
+    }
+}
